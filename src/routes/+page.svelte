87cb7dac421ebdf42011717e9499bb8194722af4
moyashi170607 <96937973+moyashi170607@users.noreply.github.com>
2026-02-10 17:18:19 +0900

    スマホ版投稿

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,8 @@
 
 	// 写真がセットされたら実行されるリアクティブ宣言
 	$: if (photo) {
-		displayOnCanvas(photo);
+		photoUrl = URL.createObjectURL(photo);
+		console.log(photoUrl);
 	}
 
 	function handleFileChange(event: Event) {
@@ -24,28 +25,9 @@
 		}
 	}
 
-	async function displayOnCanvas(file: File) {
-		// オブジェクトURLを作成して<img>で読み込めるようにする
-		photoUrl = URL.createObjectURL(file);
-
-		// DOMの更新を待ってからCanvas操作
-		await tick();
-
-		const ctx = canvasEl.getContext('2d');
-		const img = new Image();
-		img.onload = () => {
-			// Canvasのサイズを画像のサイズに合わせる
-			canvasEl.width = img.width;
-			canvasEl.height = img.height;
-			// 描画
-			ctx?.drawImage(img, 0, 0);
-			canvasEl.classList.remove('hidden'); // 必要に応じて表示
-		};
-		img.src = photoUrl;
-	}
-
 	//カメラをリセット
 	function reset() {
+		photo = null;
 		photoUrl = null;
 	}
 
@@ -77,38 +59,40 @@
 
 		if (!isMobile) {
 			const URL = resolve('/PC/');
-			goto(URL); // スマホ用ページへ飛ばす
+			goto(URL);
 		}
 	});
 </script>
 
 <h1 class="mb-5 text-center text-4xl">ノスナップ スマホ版</h1>
 
-{#if !photo}
-	<div class="text-center">
-		<label class="custom-camera-btn">
-			カメラで撮影！
-			<input
-				bind:this={fileInput}
-				type="file"
-				capture="environment"
-				accept="image/*"
-				style="display:none;"
-				on:change={handleFileChange}
-			/>
-		</label>
-	</div>
-{/if}
-
-<canvas bind:this={canvasEl} class="hidden"></canvas>
+<div class="text-center">
+	<label class="custom-camera-btn">
+		カメラで撮影！
+		<input
+			bind:this={fileInput}
+			type="file"
+			capture="environment"
+			accept="image/*"
+			style="display:none;"
+			on:change={handleFileChange}
+		/>
+	</label>
+</div>
 
 {#if photoUrl}
 	<div class="mt-10 text-center">
 		<h2 class="mb-2 text-xl">撮れた写真</h2>
-		<img src={photoUrl} alt="Captured" class="mx-auto max-w-sm border-4 border-white shadow-lg" />
+		<img src={photoUrl} alt="Captured" class="mx-auto max-w-xs border-4 border-white shadow-lg" />
 		<p class="mb-1">
 			画像と一緒に投稿する内容：<br />
-			<textarea name="comment" cols="50" rows="5" bind:value={content}></textarea>
+			<textarea
+				name="comment"
+				class="mx-auto h-full max-h-64 max-w-xs"
+				cols="50"
+				rows="5"
+				bind:value={content}
+			></textarea>
 		</p>
 		<div class="mb-5 text-center">
 			<button class="btn-camera" on:click={post_btn_clicked}> 投稿 </button>
